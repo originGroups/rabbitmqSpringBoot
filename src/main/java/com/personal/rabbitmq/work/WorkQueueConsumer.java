65577726dc73fc7b7cc11354054d034df46946ed
yuanqiang <12345678>
2020-10-18 21:30:40 +0800

    spring boot整合rabbitmq第二种模型:工作队列模型workQueue,默认为循环消费,实现能者多劳形式需要额外配置

--- src/main/java/com/personal/rabbitmq/work/WorkQueueConsumer.java
+++ src/main/java/com/personal/rabbitmq/work/WorkQueueConsumer.java
@@ -1,7 +1,6 @@
 package com.personal.rabbitmq.work;
 
 import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
